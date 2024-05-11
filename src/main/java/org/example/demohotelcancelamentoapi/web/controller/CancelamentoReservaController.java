c108410c865d55e1a5a9d50adc98efa3da697686
Lucas <lucas1115guy@gmail.com>
2024-05-11 10:00:23 -0300

    Altera swagger

--- src/main/java/org/example/demohotelcancelamentoapi/web/controller/CancelamentoReservaController.java
+++ src/main/java/org/example/demohotelcancelamentoapi/web/controller/CancelamentoReservaController.java
@@ -6,10 +6,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
-import org.example.demohotelcancelamentoapi.entity.Reserva;
 import org.example.demohotelcancelamentoapi.service.CancelamentoReservaService;
-import org.example.demohotelcancelamentoapi.web.dto.CancelamentoReservaResponseDto;
-import org.example.demohotelcancelamentoapi.web.dto.mapper.CancelamentoReservaMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,18 +23,13 @@ public class CancelamentoReservaController {
             responses = {
                     @ApiResponse(responseCode = "204", description = "Cancela as reservas com base no id",
                             content = @Content(mediaType = "application/json",
-                                    array = @ArraySchema(schema = @Schema(implementation = CancelamentoReservaResponseDto.class))))
+                                    array = @ArraySchema(schema = @Schema(implementation = Void.class))))
             }
     )
     @PatchMapping("/{reservaId}")
     public ResponseEntity<Void> cancelarReserva(@PathVariable int reservaId) {
         cancelamentoReservaService.cancelarReserva(reservaId);
         return ResponseEntity.noContent().build();
-    }
-
-    @GetMapping
-    public String getTest() {
-        return "Hotel Cancelamento Reserva API";
     }
 
 }
